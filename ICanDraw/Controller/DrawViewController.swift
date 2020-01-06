dedaff6f02abd05e2e9b0f30641718a661524b78
João Batista <j.batista.romao@gmail.com>
2020-01-06 19:46:03 -0300

    Preparado o ambiente de trabalho
    Foi preparado o ambiente de trabalho ao inserir o arquvios .travis.yml que corresponde as configuracoes utilizadas pelo Travis CI, como schema do projeto, simulador dentre outros. O arquivo .swiftlint.yml que também e responsavel pelo swiftLit e por fim o arquivo PODFILE, que é são os PODs intalados no projeto sendo estes Nimble, Quick, Nimble-Snapshots. Tambem foram criadas pastas para o modelo MVC sendo estas Model, View e Controller e uma para delegates.

---
+++ ICanDraw/Controller/DrawViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DrawViewController.swift
+//  ICanDraw
+//
+//  Created by Joao Batista on 06/01/20.
+//  Copyright © 2020 Joao Batista. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-ICanDraw-ICanDrawTests/Pods-ICanDraw-ICanDrawTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ICanDraw_ICanDrawTests : NSObject
+@end
+@implementation PodsDummy_Pods_ICanDraw_ICanDrawTests
+@end
